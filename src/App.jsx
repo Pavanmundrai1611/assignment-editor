8adb82cd1eb6087b7f033da26185daee33a0004e
Pavanmundrai1611 <pavanmundrai@gmail.com>
2024-02-02 23:31:33 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import 'draft-js/dist/Draft.css';
+import TextEditor from './TextEditor';
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}><span style={{ fontWeight: '200', color: 'red' }}>EDITOR </span>
+        <span style={{ fontWeight: '200', textDecoration: 'underline' }}>USING</span>
+        <span > DRAFT-JS</span></h1>
+      <TextEditor />
+      <div style={{textAlign:'center'}}>
+        <h3>Made by: Pavan Kumar Mundrai</h3>
+        <h3>portfolio: <a href="https://pavan-s-portfolio-c5bf5.web.app/" target='_blank'>click here</a></h3>
+        <h3>mail: <a href="mailto:pavanmundrai@gmail.com">pavanmundrai@gmail.com</a></h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/TextEditor.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Editor,
+  EditorState,
+  CompositeDecorator,
+  convertToRaw,
+  convertFromRaw,
+} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+const findWithRegex = (regex, contentBlock, callback) => {
+  const text = contentBlock.getText();
+  let matchArr, start;
+
+  while ((matchArr = regex.exec(text)) !== null) {
+    start = matchArr.index + matchArr[0].length;
+    callback(start, text.length);
+  }
+};
+
+const RedTextDecorator = {
+  strategy: (contentBlock, callback) => {
+    findWithRegex(/^\*\*\ /g, contentBlock, callback);
+  },
+  component: ({ children }) => <span style={{ color: 'red' }}>{children}</span>,
+};
+
+const UnderlineTextDecorator = {
+  strategy: (contentBlock, callback) => {
+    findWithRegex(/^\*\*\*\ /gm, contentBlock, callback);
+  },
+  component: ({ children }) => (
+    <span style={{ textDecoration: 'underline', color: 'black' }}>
+      {children}
+    </span>
+  ),
+};
+
+const HashTextDecorator = {
+  strategy: (contentBlock, callback) => {
+    findWithRegex(/^\#\ /gm, contentBlock, callback);
+  },
+  component: ({ children }) => (
+    <span>
+      <h1 style={{ color: 'black', display: 'inline' }}>{children}</h1>
+    </span>
+  ),
+};
+
+const BoldTextDecorator = {
+  strategy: (contentBlock, callback) => {
+    findWithRegex(/^\*\ /gm, contentBlock, callback);
+  },
+  component: ({ children }) => (
+    <span style={{ color: 'black' }}>
+      <b>{children}</b>
+    </span>
+  ),
+};
+
+const TextEditor = () => {
+  const decorator = new CompositeDecorator([
+    RedTextDecorator,
+    UnderlineTextDecorator,
+    HashTextDecorator,
+    BoldTextDecorator,
+  ]);
+
+  const [editorState, setEditorState] = useState(() => {
+    const savedContent = localStorage.getItem('editorContent');
+    if (savedContent) {
+      const contentState = convertFromRaw(JSON.parse(savedContent));
+      return EditorState.createWithContent(contentState, decorator);
+    }
+    return EditorState.createEmpty(decorator);
+  });
+
+  useEffect(() => {
+    const contentState = editorState.getCurrentContent();
+    const rawContentState = convertToRaw(contentState);
+    localStorage.setItem('editorContent', JSON.stringify(rawContentState));
+  }, []);  
+
+  const onChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+
+  const handleSaveClick = () => {
+    const contentState = editorState.getCurrentContent();
+    const rawContentState = convertToRaw(contentState);
+    localStorage.setItem('editorContent', JSON.stringify(rawContentState));
+  };
+
+  return (
+    <div className="editor">
+      <div className='editor-container'>
+        <Editor editorState={editorState} onChange={onChange} />
+      </div>
+      <br />
+      <button onClick={handleSaveClick}>Save</button>
+    </div>
+  );
+};
+
+export default TextEditor;
